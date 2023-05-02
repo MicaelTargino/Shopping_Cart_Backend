a56b39c20d0c9319f99321637fd1e6479899cdc0
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-05-02 14:54:11 -0300

    token authentication

--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from core.views import ListViewSet, ItemViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'lists', ListViewSet)
+router.register(r'items', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
