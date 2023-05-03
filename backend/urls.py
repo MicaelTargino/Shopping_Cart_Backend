df48a0d0131d712e32ef04581bfd6972cab76d19
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-05-03 13:17:32 -0300

    config dynamic search auth

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken import views as viewsSYS
 from . import views
 from core.views import ListViewSet, ItemViewSet
 
@@ -12,5 +13,6 @@ router.register(r'items', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth', viewsSYS.obtain_auth_token, name="api-token-auth"),
     path('admin/', admin.site.urls),
 ]
